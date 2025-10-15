fae10987f6dbcca61912fb989d25526827bcf314
Ben Montgomery <ben@modernthings.net>
2025-10-15 14:47:30 -0600

    Projects and Tags from Firestore to Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { useCollection } from 'vuefire'
-import { timeBoxesRef } from '@/firebase'
-
 import TimeBox from '@/components/TimeBox.vue'
-const timeBoxes = useCollection(timeBoxesRef)
+import { timeBoxes } from '@/firebase'
 </script>
 
 <template>
@@ -14,5 +11,7 @@ const timeBoxes = useCollection(timeBoxesRef)
     :startTime="item.startTime.toDate()"
     :endTime="item.endTime.toDate()"
     :notes="item.notes"
+    :project="item.project"
+    :tags="item.tags"
   />
 </template>
--- src/components/TimeBox.vue
+++ src/components/TimeBox.vue
@@ -1,16 +1,50 @@
 <script setup lang="ts">
-defineProps({
+import { computed } from 'vue'
+
+import { projects } from '@/firebase'
+import { allTags } from '@/firebase'
+
+const props = defineProps({
   startTime: Date,
   endTime: Date,
   notes: String,
+  project: String,
+  tags: Array,
+})
+
+const projectName = computed(() => {
+  let computedProjectName: string = ''
+
+  projects.value.forEach((thisProject) => {
+    if (thisProject.id === props.project) {
+      computedProjectName = thisProject.name
+    }
+  })
+
+  return computedProjectName
+})
+
+const tagNames = computed(() => {
+  const computedTagNames: string[] = []
+
+  allTags.value.forEach((thisTag) => {
+    props.tags?.forEach((thisPropsTag) => {
+      if (thisTag.id === thisPropsTag) {
+        computedTagNames.push(thisTag.name)
+      }
+    })
+  })
+
+  return computedTagNames
 })
 </script>
 
 <template>
   <div class="my-2 bg-gray-200 p-8">
-    <h1 class="text-4xl">TimeBox</h1>
     <div class="mt-2">{{ startTime }}</div>
     <div class="mt-2">{{ endTime }}</div>
     <div class="mt-2">{{ notes }}</div>
+    <div class="mt-2">{{ projectName }}</div>
+    <div class="mt-2">{{ tagNames }}</div>
   </div>
 </template>
