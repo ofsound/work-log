256960cfa237d0f9df86e33f598f63535ea8908f
Ben Montgomery <ben@modernthings.net>
2025-10-15 11:43:35 -0600

    Get a firestore in here from the start

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import './main.css'
 
 
 import { createApp } from 'vue'
+import { VueFire, VueFireAuth } from 'vuefire'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { firebaseApp } from './firebase.ts'
+
 
 const app = createApp(App)
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [
+    VueFireAuth(),
+  ],
+})
 
 app.use(createPinia())
 app.use(router)
