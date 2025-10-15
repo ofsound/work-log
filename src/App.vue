7f3959328cf67c91378acf5a0f0313c84ff84c6f
Ben Montgomery <ben@modernthings.net>
2025-10-15 11:26:40 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TimeBox from '@/components/TimeBox.vue'
+</script>
+
+<template>
+  <h1 class="text-4xl">Work Log</h1>
+  <p class="mt-4">Lock in</p>
+  <TimeBox v-for="n in 10" :key="n" />
+</template>
+
+<style scoped></style>
---
+++ src/components/TimeBox.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-4xl">TimeBox</h1>
+  <p class="mt-4">here are my details</p>
+</template>
