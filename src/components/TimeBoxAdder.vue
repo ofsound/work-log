07a2a3090556977bd3d1bcaff7f9707013e4a5ec
Ben Montgomery <ben@modernthings.net>
2025-10-15 19:00:25 -0600

    Adder sends data from form to Firestore

--- src/components/TimeBoxAdder.vue
+++ src/components/TimeBoxAdder.vue
@@ -1,16 +1,37 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-import { allProjects, allTags } from '@/firebase'
+import { addDoc, Timestamp } from 'firebase/firestore'
+import { allProjects, allTags, timeBoxesCollection } from '@/firebase'
 
 const startTime = ref()
 const endTime = ref()
 const notes = ref()
-const projectName = ref()
-const tagNames = ref([])
+const project = ref()
+const tags = ref([])
 
 const clickHandler = () => {
-  console.log('create a new timeBox in Firestore')
+  createDocument()
+  // console.log('create a new timeBox in Firestore')
+}
+
+async function createDocument() {
+  try {
+    // Get a reference to the collection
+    // const colRef = collection(db, 'your_collection_name')
+    // Use addDoc to create a new document with an auto-generated ID
+
+    const docRef = await addDoc(timeBoxesCollection, {
+      startTime: Timestamp.fromDate(new Date(startTime.value)),
+      endTime: Timestamp.fromDate(new Date(endTime.value)),
+      notes: notes.value,
+      project: project.value,
+      tags: tags.value,
+    })
+    console.log('Document added with ID: ', docRef.id)
+  } catch (e) {
+    console.error('Error adding document: ', e)
+  }
 }
 </script>
 
@@ -20,18 +41,18 @@ const clickHandler = () => {
     <div>endTime: <input type="date" v-model="endTime" placeholder="start" /></div>
     <div>notes: <textarea v-model="notes" class="w-full"></textarea></div>
     <div>
-      projectName:
-      <select v-model="projectName">
+      project:
+      <select v-model="project">
         <option v-for="thisProject in allProjects" :key="thisProject.id" :value="thisProject.id">
           {{ thisProject.name }}
         </option>
       </select>
     </div>
     <div>
-      tagNames:
+      tags:
       <div v-for="thisTag in allTags" :key="thisTag.id">
         {{ thisTag.name }}
-        <input type="checkbox" :value="thisTag.id" v-model="tagNames" />
+        <input type="checkbox" :value="thisTag.id" v-model="tags" />
       </div>
     </div>
     <button @click="clickHandler" class="mt-2 border border-black bg-green-300 p-2">Submit</button>
