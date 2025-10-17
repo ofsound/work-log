7df153709494fb21f7f599492499dd6a4bbe0b92
Ben Montgomery <ben@modernthings.net>
2025-10-17 15:22:23 -0600

    Formatted data in TimeBox

--- src/components/TimeBox.vue
+++ src/components/TimeBox.vue
@@ -17,7 +17,7 @@ const props = defineProps({
 })
 
 const projectName = computed(() => {
-  let computedProjectName: string = ''
+  let computedProjectName = ''
 
   allProjects.value.forEach((thisProject) => {
     if (thisProject.id === props.project) {
@@ -41,14 +41,52 @@ const tagNames = computed(() => {
 
   return computedTagNames
 })
+
+const timeBoxDuration = computed(() => {
+  let durationString = 0
+
+  if (props.endTime && props.startTime) {
+    durationString = (props.endTime.valueOf() - props.startTime.valueOf()) / 60000
+  }
+
+  return durationString
+})
+
+const startDayFormatted = computed(() => {
+  return props.startTime?.toLocaleDateString([], {
+    weekday: 'short',
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+  })
+})
+
+const startTimeFormatted = computed(() => {
+  return props.startTime?.toLocaleTimeString([], {
+    hourCycle: 'h23',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+})
+
+const endTimeFormatted = computed(() => {
+  return props.endTime?.toLocaleTimeString([], {
+    hourCycle: 'h23',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+})
 </script>
 
 <template>
   <div class="my-2 bg-gray-200 p-4">
-    <div class="mt-2">{{ startTime }}</div>
-    <div class="mt-2">{{ endTime }}</div>
+    <div class="mt-2">{{ timeBoxDuration }}</div>
+    <div class="mt-2">{{ startDayFormatted }}</div>
+    <div class="mt-2">{{ startTimeFormatted }} – {{ endTimeFormatted }}</div>
     <div class="mt-2">{{ notes }}</div>
     <div class="mt-2">{{ projectName }}</div>
-    <div class="mt-2">{{ tagNames }}</div>
+    <div class="mt-2">
+      <div v-for="thisTag in tagNames" :key="thisTag">{{ thisTag }}</div>
+    </div>
   </div>
 </template>
